eeb859424ac49b308aa64afb4e4ea100e74d0b00
Sergey Tunnik <traaance@gmail.com>
2016-03-02 01:26:41 +0200

    Added more tests.

--- Tests/ST.ServiceBus.Powershell.Tests.ps1
+++ Tests/ST.ServiceBus.Powershell.Tests.ps1
@@ -10,6 +10,33 @@ $testsNamespaceManager = [Microsoft.ServiceBus.NamespaceManager]::CreateFromConn
 SetupTestsFixture -NamespaceManager $testsNamespaceManager
 
 
+Describe 'Add-SBRule' {
+    It 'successfully adds rule using name and filter' {
+        $sc = Get-SBSubscriptionClient -ConnectionString $testsConnectionString -TopicPath 'test-topic1' -Name 'test-subscr1'
+        Add-SBRule -SubscriptionClient $sc -Name 'test-rule1' -Filter 'Priority > 42'
+        $allrules = Get-SBRule -NamespaceManager $testsNamespaceManager -TopicPath 'test-topic1' -SubscriptionName 'test-subscr1'
+        $exactrule = $allrules | Where-Object -FilterScript { $_.Name -eq 'test-rule1' }
+        $exactrule | Should BeOfType [Microsoft.ServiceBus.Messaging.RuleDescription]
+        $exactrule.Filter.SqlExpression | Should BeExactly 'Priority > 42'
+    }
+    
+    It 'successfully adds rule using RuleDescription with action' {
+        $sc = Get-SBSubscriptionClient -ConnectionString $testsConnectionString -TopicPath 'test-topic1' -Name 'test-subscr1'
+        $rd = New-SBRuleDescription -Name 'test-rule2' -Filter 'Size < 12' -RuleAction 'Set TextSize = "S"'
+        Add-SBRule -SubscriptionClient $sc -RuleDescription $rd
+        $allrules = Get-SBRule -NamespaceManager $testsNamespaceManager -TopicPath 'test-topic1' -SubscriptionName 'test-subscr1'
+        $exactrule = $allrules | Where-Object -FilterScript { $_.Name -eq 'test-rule2' }
+        $exactrule | Should BeOfType [Microsoft.ServiceBus.Messaging.RuleDescription]
+        $exactrule.Filter.SqlExpression | Should BeExactly 'Size < 12'
+        $exactrule.Action.SqlExpression | Should BeExactly 'Set TextSize = "S"'
+    }
+    
+    It 'throws when adding rule duplicated name' {
+        $sc = Get-SBSubscriptionClient -ConnectionString $testsConnectionString -TopicPath 'test-topic1' -Name 'test-subscr1'
+        { Add-SBRule -SubscriptionClient $sc -Name 'test-rule1' -Filter 'Priority > 42' } | Should Throw
+    }
+}
+
  Describe 'Get-SBNamespaceManager' {
      It 'throws on bad connection string' {
          { Get-SBNamespaceManager -ConnectionString 'Bad;connection;string' } | Should Throw
@@ -36,22 +63,22 @@ SetupTestsFixture -NamespaceManager $testsNamespaceManager
 
  
 Describe 'Get-SBQueue' {
-    it 'gets all queues' {
+    It 'gets all queues' {
         $q = Get-SBQueue -NamespaceManager $testsNamespaceManager
         $q.Count | Should Be 3
     }
     
-    it 'gets specific queue' {
+    It 'gets specific queue' {
         $q = Get-SBQueue -NamespaceManager $testsNamespaceManager -Path 'test-queue1'
         $q.Count | Should Be 1
         $q[0].Path | Should BeExactly 'test-queue1'
     }
     
-    it 'can''t find specific queue' {
+    It 'can''t find specific queue' {
         { $q = Get-SBQueue -NamespaceManager $testsNamespaceManager -Path 'imnotpresent' } | Should Throw
     }
     
-    it 'gets queues by filtering' {
+    It 'gets queues by filtering' {
         $q = Get-SBQueue -NamespaceManager $testsNamespaceManager -Filter 'createdAt gt ''1999-01-01'''
         $q.Count | Should BeGreaterThan 0
     }
@@ -59,10 +86,77 @@ Describe 'Get-SBQueue' {
 
 
 Describe 'Remove-SBQueue' {
-    it 'sucessfully removes queue' {
+    It 'successfully removes queue' {
         Test-SBQueue -NamespaceManager $testsNamespaceManager -Path 'test-queue1' | Should Be $true
         Remove-SBQueue -NamespaceManager $testsNamespaceManager -Path 'test-queue1'
         Test-SBQueue -NamespaceManager $testsNamespaceManager -Path 'test-queue1' | Should Be $false
+    }
+}
+
+
+Describe 'Remove-SBRule' {
+    It 'successfully removes rule' {
+        $allrules = Get-SBRule -NamespaceManager $testsNamespaceManager -TopicPath 'test-topic1' -SubscriptionName 'test-subscr1'
+        $exactrule = $allrules | Where-Object -FilterScript { $_.Name -eq 'test-rule1' }
+        $exactrule | Should Not BeNullOrEmpty
+        
+        $sc = Get-SBSubscriptionClient -ConnectionString $testsConnectionString -TopicPath 'test-topic1' -Name 'test-subscr1'
+        Remove-SBRule -SubscriptionClient $sc -Name 'test-rule1'
+        
+        $allrules = Get-SBRule -NamespaceManager $testsNamespaceManager -TopicPath 'test-topic1' -SubscriptionName 'test-subscr1'
+        $exactrule = $allrules | Where-Object -FilterScript { $_.Name -eq 'test-rule1' }
+        $exactrule | Should BeNullOrEmpty
+    } 
+}
+
+
+Describe 'Remove-SBSubscription' {
+    It 'successfully removes subscription' {
+        Test-SBSubscription -NamespaceManager $testsNamespaceManager -TopicPath 'test-topic3' -Name 'test-subscr3' | Should Be $true
+        Remove-SBSubscription -NamespaceManager $testsNamespaceManager -TopicPath 'test-topic3' -Name 'test-subscr3'
+        Test-SBSubscription -NamespaceManager $testsNamespaceManager -TopicPath 'test-topic3' -Name 'test-subscr3' | Should Be $false
+    }
+}
+
+
+Describe 'Remove-SBTopic' {
+    It 'successfully removes topic' {
+        Test-SBTopic -NamespaceManager $testsNamespaceManager -Path 'test-topic3' | Should Be $true
+        Remove-SBTopic -NamespaceManager $testsNamespaceManager -Path 'test-topic3'
+        Test-SBTopic -NamespaceManager $testsNamespaceManager -Path 'test-topic3' | Should Be $false
+    }
+}
+
+
+Describe 'Test-SBQueue' {
+    It 'gets positive check' {
+        Test-SBQueue -NamespaceManager $testsNamespaceManager -Path 'test-queue3' | Should Be $true
+    }
+    
+    It 'gets negative check' {
+        Test-SBQueue -NamespaceManager $testsNamespaceManager -Path 'nonexistqueue' | Should Be $false
+    }
+}
+
+
+Describe 'Test-SBSubscription' {
+    It 'gets positive check' {
+        Test-SBSubscription -NamespaceManager $testsNamespaceManager -TopicPath 'test-topic2' -Name 'test-subscr2' | Should Be $true
+    }
+    
+    It 'gets negative check' {
+        Test-SBSubscription -NamespaceManager $testsNamespaceManager -TopicPath 'nonexisttopic' -Name 'nonexistsubscr' | Should Be $false
+    }
+}
+
+
+Describe 'Test-SBTopic' {
+    It 'gets positive check' {
+        Test-SBTopic -NamespaceManager $testsNamespaceManager -Path 'test-topic2' | Should Be $true
+    }
+    
+    It 'gets negative check' {
+        Test-SBTopic -NamespaceManager $testsNamespaceManager -Path 'nonexisttopic' | Should Be $false
     }
 }
 
